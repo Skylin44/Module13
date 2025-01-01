d46724fadcc1fc65d08882f5c83a5646c9b2ccc8
Елена <pox4not@gmail.com>
2025-01-01 13:43:39 +0300

    "Методы отправки сообщений".

---
+++ module_13/module_13_3.py
@@ -0,0 +1,23 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from kye import api
+import asyncio
+
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+@dp.message_handler(commands=['help'])
+async def send_welcome(message: types.Message):
+    await message.answer("Я бот обратной связи для учёбы")
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.answer("Привет, я бот помогающий твоему здоровью.")
+
+@dp.message_handler()
+async def all_messages(message: types.Message):
+    await message.answer("Введите команду /start, для начала работы")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
