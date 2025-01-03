2a870caaef5f7327c244235b506abc13b790fa0a
Елена <pox4not@gmail.com>
2025-01-03 13:46:29 +0300

    "Машина состояний"

---
+++ module_13/module_13_4.py
@@ -0,0 +1,51 @@
+from aiogram import Bot, Dispatcher, executor, types
+import asyncio
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+
+
+TOKEN=""
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.answer("Привет, я бот помогающий твоему здоровью.")
+
+
+@dp.message_handler(text='Calories')
+async def set_age(message, state):
+    await message.answer('Введите свой возраст: ')
+    await UserState.age.set()
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await UserState.growth.set()
+    await message.answer('Введите свой рост: ')
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await UserState.weight.set()
+    await message.answer('Введите свой вес: ')
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    calories = 10 * int(data['weight']) + 6.25 * int(data['growth']) - 5 * int(data['age']) - 161
+    await message.answer(f'Ваша норма калорий: {calories}')
+    await state.finish()
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
